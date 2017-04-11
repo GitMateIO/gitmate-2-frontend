aa09fe87174aadc5c4c8c932d5326bd2c542845c
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-04-11 15:18:45 +0200

    Add toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 
 import { MdButtonModule } from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdMenuModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MdButtonModule
+    MdButtonModule,
+    MdToolbarModule,
+    MdMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+//import { ApiService } from './../../services';
+import { UserModel } from './../models';
+
+@Component({
+  selector: 'gm-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css'],
+})
+export class ToolbarComponent {
+  user: UserModel;
+//  constructor(private apiService: ApiService) {}
+
+ // ngOnInit() {
+ //   this.apiService.getUser().subscribe(user => this.user = user);
+ // }
+
+  userDefined() {
+    return !(this.user == null);
+  }
+
+  loginGitHub() {
+//    window.location.href = '/auth/login/github';
+  }
+
+  logout() {
+//    window.location.href = '/logout';
+  }
+}
