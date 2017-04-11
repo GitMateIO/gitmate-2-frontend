920b633761925a933e204e1593f56bf10e722b5a
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-04-11 14:21:19 +0200

    theme: Add angular material css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
+import { MdButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
