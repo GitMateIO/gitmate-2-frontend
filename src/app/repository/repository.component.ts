ada24e82ca3f311a1d39b27c1611ec73374adac0
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-04-18 11:21:04 +0200

    repo-view: Update de/activated repositories
    otherwise, if the checkbox is ticket to show only active repositories,
    the selection is made on top of an old state

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,  EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 import { ApiService } from './../api/api.service';
 import { RepoModel } from './../models';
@@ -9,17 +9,22 @@ import { RepoModel } from './../models';
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
-@Input()
+  @Output()
+  active_toggled: EventEmitter<string> = new EventEmitter();
+
+  @Input()
   repo: RepoModel;
 
   constructor(private apiService: ApiService) { }
 
   enable() {
     this.apiService.addRepo(this.repo.id).subscribe(repo => this.repo = repo);
+    this.active_toggled.emit('enabled');
   }
 
   disable() {
     this.apiService.removeRepo(this.repo.id).subscribe(repo => this.repo = repo);
+    this.active_toggled.emit('disabled');
   }
 
   ngOnInit() {
