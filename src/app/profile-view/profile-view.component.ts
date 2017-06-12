8cb82987d017d5b011bb7aa91eee8592bdc190f6
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-06-12 12:29:19 +0200

    profile-view: Show hint for invalid values

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../api/api.service';
 import { UserModel } from './../models';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-profile-view',
@@ -9,6 +10,8 @@ import { UserModel } from './../models';
 })
 export class ProfileViewComponent implements OnInit {
   user: UserModel;
+  user_name_message: string;
+  user_email_message: string;
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
@@ -17,12 +20,22 @@ export class ProfileViewComponent implements OnInit {
 
   setEmail() {
     this.apiService.patch('/api/users/me/', {'email': this.user.email})
-      .subscribe(user => this.user = user);
+      .subscribe(
+        user => {
+          this.user = user;
+          this.user_email_message = '';
+        },
+        err => this.user_email_message = err.json().email[0]);
   }
 
   setUserName() {
     this.apiService.patch('/api/users/me/', {'username': this.user.username})
-      .subscribe(user => this.user = user);
+      .subscribe(
+        user => {
+          this.user = user;
+          this.user_name_message = '';
+        },
+        err => this.user_name_message = err.json().username[0]);
   }
 
 }
