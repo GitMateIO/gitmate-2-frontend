d9398bd7fd432f8e2551f1288e1924b1ecedc0bb
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-06-15 13:02:14 +0200

    home-view: Add call to action buttons

--- src/app/home-view/home-view.component.ts
+++ src/app/home-view/home-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from './../../environments/environment';
+import { ApiService } from './../api/api.service';
+import { UserModel } from './../models';
 
 @Component({
   selector: 'app-home-view',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-view.component.css']
 })
 export class HomeViewComponent implements OnInit {
-
-  constructor() { }
+  user: UserModel;
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    this.apiService.getUser().subscribe(user => this.user = user);
+  }
+
+  userDefined() {
+    return !(this.user == null);
+  }
+
+  loginGitHub() {
+    window.location.href = environment.backend_url + '/auth/login/github/';
+  }
+
+
+  loginGitLab() {
+    window.location.href = environment.backend_url + '/auth/login/github/';
   }
 
 }
