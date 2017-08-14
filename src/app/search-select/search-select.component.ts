e29db8a9bff3926fa13f482394b99b048a45ddf2
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-08-14 17:03:34 +0200

    search-select: Implementation

--- src/app/search-select/search-select.component.ts
+++ src/app/search-select/search-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-select',
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-select.component.css']
 })
 export class SearchSelectComponent implements OnInit {
+  @ViewChild('search') searchElementRef;
+  @ViewChild('auto') autocompleteElementRef;
+  @Input() choices: any[];
+  @Input() placeholder: string;
+  @Output() selected = new EventEmitter<string>();
+  inputValue = '';
+  filteredResults: any[];
 
-  constructor() { }
+  constructor() {}
+
+  filterResults(val: string) {
+    return this.choices.filter(t => !val || (t.toLowerCase().indexOf(val.toLowerCase()) !== -1));
+  }
+
+  onSelect(event, item) {
+    // this event is raised for selected and deselected items.
+    // we can differentiate them by event.isUserInput..
+    if (event.isUserInput) {
+      this.selected.emit(item);
+    }
+  }
+
+  onKeyDown(event) {
+    if (event.key === 'Enter') {
+      const results = this.filterResults(this.searchElementRef.nativeElement.value);
+      if (results) {
+        this.inputValue = '';
+        this.filteredResults = [];
+        this.selected.emit(results[0]);
+      }
+    } else if (event.key === 'Escape') {
+      this.filteredResults = [];
+      this.searchElementRef.nativeElement.blur();
+    }
+  }
+
+  onClear() {
+    this.inputValue = '';
+  }
 
   ngOnInit() {
   }
-
 }
