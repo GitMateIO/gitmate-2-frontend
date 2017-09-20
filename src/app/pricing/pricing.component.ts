5a9cdb16c0748a558af0f88a92da77e4de1f65e9
Hemang Kumar <hemangsk@gmail.com>
2017-09-20 21:16:10 +0530

    pricing: Add layout

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MdGridListModule} from '@angular/material';
+
 
 @Component({
   selector: 'app-pricing',
@@ -11,5 +13,26 @@ export class PricingComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  pricing = [
+	  {
+	  	"title":"plan 1",
+	  	"highlights" : ["h1", "h2", "h3"],
+	  	"features" : ["f1", "f2", "f3"],
+	  	"price" : "Free"
+	  },
+	  {
+	  	"title":"plan 1",
+	  	"highlights" : ["h1", "h2", "h3"],
+	  	"features" : ["f1", "f2", "f3", "f4", "f5"],
+	  	"price" : "30E/month"
+	  },
+	  {
+	  	"title":"plan 1",
+	  	"highlights" : ["h1", "h2", "h3"],
+	  	"features" : ["f1", "f2", "f3"],
+	  	"price" : "50E/month"
+	  }
+  ]
 
 }
