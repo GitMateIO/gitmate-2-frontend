10e599eba88d8dee6bd300c755916296fc768107
Hemang Kumar <hemangsk@gmail.com>
2017-09-30 20:13:22 +0530

    features: Use correct paths for visuals

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -19,7 +19,7 @@ export class FeaturesComponent implements OnInit {
                         GitMate will post a link to the likely duplicates in a comment of the new issue.\
                         This helps maintainers to find and remove duplicates. Users instantly find \
                         helpful information in the discussions of the linked similar issues.",
-        "img" : "feature0.png"
+        "img" : "issue_duplicate.png"
       },
       {
         "title" : "Setting labels automatically",
@@ -27,7 +27,7 @@ export class FeaturesComponent implements OnInit {
                         GitMate will automatically set appropriate labels based on the conventions you are already using. \
                         The calculation is based on labels you have set in the past and continuously improves.<p> \
                         <p>Furthermore, keywords can be set up to trigger the assignment of specific labels.</p>",
-        "img" : "feature4.png"
+        "img" : "tagging_issues.png"
       },
       {
         "title" : "Determining Missing Information (Coming Soon!)",
@@ -36,7 +36,7 @@ export class FeaturesComponent implements OnInit {
                         mark the issue as \"needs-info\" and ask the user for it.<p> \
                         <p>This way you never have to worry about incomplete issue reports again.</p> \
                         <p>We are currently working hard to make this feature reality, it will come soon - promised!</p>",
-        "img" : "feature2.png"
+        "img" : "find_missing_info.png"
       },
       {
         "title" : "Calling for help",
@@ -44,7 +44,7 @@ export class FeaturesComponent implements OnInit {
                         part of the project or a specific kind off issue, based on their previous involvement. \
                         By mentioning appropriate contributors in the issue discussion, GitMate notifies them of\
                         the issues they need to know about.",
-        "img" : "feature1.png"
+        "img" : "call_for_help.png"
       },
       {
         "title" : "Handling stale issues",
@@ -53,7 +53,7 @@ export class FeaturesComponent implements OnInit {
                         include setting a label, posting a message, unassigning inactive developers, and eventually \
                         closing the issue.</p>\
                         <p>The exact behaviour is dependend on labels set on the issue and can be extensively configured</p>",
-        "img" : "feature3.png"
+        "img" : "stale_issues.png"
       }
   ]
 }
