04218f9dfdfe8b79733a8cf1140632007d649b9f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-10-13 12:18:13 -0700

    Add new plans

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -27,27 +27,45 @@ export class PricingComponent implements OnInit {
   pricing = [
     {
       "title":"Basic",
-      "features" : ["Limited to 8 users per organization"],
-      "price": {"amount": "Free", "limit": null},
+      "features": [
+        "Handle up to 50 issues/month",
+        "Limited to 8 users per organization",
+        "Community Support",
+      ],
+      "price": {"amount": "Free", "limit": "always!"},
       "height": 430
     },
     {
       "title":"Professional",
       "features": [
-        "Limited to 200 users per organization",
-        "Priority Support"
+        "Handle up to 200 issues/month",
+        "Unlimited users",
+        "Priority Support",
       ],
-      "price" : {"amount": "29€", "limit": "user/month"},
+      "price" : {"amount": "29€", "limit": "/month"},
       "height": 480
     },
     {
       "title":"Enterprise",
       "features": [
+        "Handle up to 500 issues/month",
         "Unlimited users",
         "Priority Support",
         "On-premises deployment"
       ],
-      "price" : {"amount": "49€", "limit": "user/month"},
+      "price" : {"amount": "99€", "limit": "/month"},
+      "height": 430
+    },
+    {
+      "title":"Unicorn",
+      "features": [
+        "More than 500 issues/month",
+        "Unlimited users",
+        "Priority Support",
+        "Issue Analytics",
+        "On-premises deployment"
+      ],
+      "price" : {"amount": "contact us", "limit": "/month"},
       "height": 430
     }
   ]
